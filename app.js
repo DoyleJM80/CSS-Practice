6a0e17b1d644bc6094ceb2566c6cef8008143f08
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-09-27 13:26:22 -0400

    added event listeners to inputs

---
+++ app.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    let inputs = document.querySelectorAll('.controls input');
+
+    function handleUpdate() {
+        console.log(this.value);
+    };
+
+    inputs.forEach((input) => input.addEventListener('change', handleUpdate));
+    inputs.forEach((input) => input.addEventListener('mousemove', handleUpdate));
+
+
+
+
+}());
